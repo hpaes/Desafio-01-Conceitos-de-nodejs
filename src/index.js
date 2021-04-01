8d611011361e65263f71a0c331dd1f6b208c6687
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-04-01 18:30:43 -0300

    finished challlenge

--- src/index.js
+++ src/index.js
@@ -75,6 +75,10 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 
   const todo = user.todos.find((todo) => todo.id === id);
 
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
   todo.title = title;
   todo.deadline = new Date(deadline);
 
@@ -82,11 +86,33 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const findTodoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (findTodoIndex < 0) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  user.todos.splice(findTodoIndex, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
